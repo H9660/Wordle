902ccf6caf37ad9b992c96c1fd120b698a84e9e4
Kayla <kay_kay1424@yahoo.com>
2023-10-15 17:59:53 -0500

    Add theme feature functionality

--- src/Themesetter.js
+++ src/Themesetter.js
@@ -3,7 +3,21 @@ import { useState } from "react";
 import Select from "react-select";
 import { generateAnimalset, generateCitiesSet, generateWordSet } from "./words";
 
-function Themesetter() {
+function Themesetter(props) {
+  var themes = {
+    Animals: {
+      color: "green",
+      getWordSet: generateAnimalset,
+    },
+    Cities: {
+      color: "orange",
+      getWordSet: generateCitiesSet,
+    },
+    General: {
+      color: "cyan",
+      getWordSet: generateWordSet,
+    },
+  };
   var colors = [
     {
       value: 1,
@@ -23,17 +37,29 @@ function Themesetter() {
   ];
 
   var [bgimage, setbgimage] = useState("black");
-  var bgimagehandler = (e) => {
-    setbgimage(e.label);
+  var bgimagehandler = async (e) => {
+    var theme = themes[e.label];
+    setbgimage(theme.color);
+    var newWordSet = await theme.getWordSet();
+    props.changeWordSet(newWordSet);
   };
   return (
     <div id="selectbutton">
-      <style>{"body {background-color:" + bgimage + " ;}"}</style>
+      <style>{"#body {background-color:" + bgimage + " ;}"}</style>
       <center>
         <Select
-          options={colors}
+          options={Object.keys(themes).map((key) => ({
+            label: key,
+            value: key.toLowerCase(),
+          }))}
           onChange={bgimagehandler}
           placeholder="Select your theme"
+          styles={{
+            option: (baseStyles, state) => ({
+              ...baseStyles,
+              color: state.isSelected ? "white" : "black",
+            }),
+          }}
         ></Select>
       </center>
     </div>
